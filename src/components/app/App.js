ac40bd18b6401ede8e8abe04518659e28dde393e
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-06-23 17:32:00 +0500

    linked

--- src/components/app/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>Fashion Project</div>
-  )
-}
-
-export default App
---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Sidebar from "../side/side";
+import Home from "../home/home";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Home />
+    </>
+  );
+};
+
+export default App;
